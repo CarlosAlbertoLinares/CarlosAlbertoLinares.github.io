cd558d22848ee6923004f5d9abfeb6a9858c48dd
Carlos Alberto Linares <50836047+ElHijodeNewtonPower@users.noreply.github.com>
2019-06-23 20:31:46 -0300

    Update App.js

--- Entrevista Pitech/ejercicio4/src/App.js
+++ Entrevista Pitech/ejercicio4/src/App.js
@@ -41,7 +41,7 @@ render(){
 <div className ="App">
 <header className = "App-header">
 <img src ={logo} className = "App-logo" alt="logo"/>
-<h1 className ="App-title" > Escriba el número siguiente con letras: </h1>
+<h1 className ="App-title" > Escriba el número siguiente con letras haciendo click en la respuesta con el orden correcto: </h1>
 </header>
 <div className = "App-intro">
 <Problem />
